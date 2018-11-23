3a8f7208e381aff4fce49533e5b7c716e24561af
shengqing <shengoo@hotmail.com>
2018-11-23 12:10:57 +0800

    log in and out

--- App/Sagas/UserSagas.js
+++ App/Sagas/UserSagas.js
@@ -1,6 +1,5 @@
 import {call, put} from 'redux-saga/effects'
 import UserAction from '../Redux/UserRedux'
-console.log(UserAction)
 
 export function* loginSuccess(action) {
      put(UserAction.loginSuccess(action.user))
